45a348819fe3645450f3ac580dbd3a43788be13b
𝐍𝐢𝐯𝐫𝐢𝐭𝐢 𝐌𝐮𝐭𝐡𝐮𝐯𝐚𝐢𝐫𝐚𝐯𝐚𝐧 <nivriti3266@gmail.com>
2025-07-11 17:05:08 +0400

    Add files via upload

---
+++ TakeName.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+class TakeName{
+      public static void main(String arg[]){
+               System.out.println("What is your name?");
+               Scanner s=new Scanner(System.in);
+               String name;
+               name=s.next();/* its gonna be a word */
+               System.out.println(" Welcome"+name);
+               }
+}
